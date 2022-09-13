5143146e7425105eb439fbe7ede483c9584a4bf7
Milan Stojko <milanmilance2000.ms@gmail.com>
2022-09-13 11:27:56 +0200

    added fontawesom

--- src/components/ui/social/Social.jsx
+++ src/components/ui/social/Social.jsx
@@ -1,22 +1,19 @@
-function Social(){
-    return(
-        <div>
-            <div>
-                <span className="sign">Or sign up using</span>
-            </div>
-            <div className="social-icons">
-                <a>
-                    <i></i>
-                </a>
-                <a>
-                    
-                </a>
-            </div>
-        </div>
-
-
-
-    )
+function Social() {
+  return (
+    <div>
+      <div>
+        <span className="sign">Or sign up using</span>
+      </div>
+      <div className="social-icons">
+        <a>
+          <i></i>
+        </a>
+        <a>
+          <i></i>
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default Social;
